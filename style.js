45c9a08337a1f80d9795e9194b63631fdfba0112
brucemorakane <brucemorakane74@gmail.com.com>
2022-09-22 08:57:22 +0200

    style.js added with for

---
+++ style.js
@@ -0,0 +1,17 @@
+var AddHome = document.getElementById('AddHome')
+var aboutlist = document.getElementById('aboutlist')
+var foodmenu = document.getElementById('foodmenu')
+var photo = document.getElementById('photo')
+var telephone = document.getElementById('telephone')
+
+var totalscreen = document.getElementById('totalscreen')
+
+screenTop = []
+
+function caculate() {
+    for (let screenTop = 0; screenTop < array.length; index++) {
+        
+    }
+}
+
+
